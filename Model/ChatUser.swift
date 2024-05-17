d47f1616e720517f96cc573bac08e1defdd3d959
Shaq-Mg <shaqmcg0@gmail.com>
2024-05-17 20:33:20 +0100

    ChatUser model created

---
+++ Model/ChatUser.swift
@@ -0,0 +1,15 @@
+//
+//  ChatUser.swift
+//  Messenger
+//
+//  Created by Shaquille McGregor on 17/05/2024.
+//
+
+import Foundation
+
+struct ChatUser: Identifiable {
+    var id = UUID()
+    let username: String
+    let email: String
+    let password: String
+}
