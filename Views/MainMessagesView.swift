882d1fb827c192d88712b6ccd56f3d7a25774dec
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-07 15:19:50 +0000

    Injecting view models to app enviroment

---
+++ Views/MainMessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MainMessagesView.swift
+//  Messenger
+//
+//  Created by Shaquille McGregor on 07/12/2024.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMessagesView()
+}
