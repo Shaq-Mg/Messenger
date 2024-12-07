a4af7e3eb1009d1a22c4b3ad9744954acb80842a
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-07 14:34:11 +0000

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Shaquille McGregor on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MessengerTests/MessengerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTests.swift
+//  MessengerTests
+//
+//  Created by Shaquille McGregor on 07/12/2024.
+//
+
+import Testing
+@testable import Messenger
+
+struct MessengerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
