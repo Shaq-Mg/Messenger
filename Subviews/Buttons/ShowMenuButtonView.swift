75caf990a6fffdd2d0a46e6dae8dc58c5a60fa44
Shaq-Mg <shaqmcg0@gmail.com>
2024-12-07 16:31:27 +0000

    Messagner UI setup & connecting firebase

---
+++ Subviews/Buttons/ShowMenuButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowMenuButtonView.swift
+//  Messenger
+//
+//  Created by Shaquille McGregor on 07/12/2024.
+//
+
+import SwiftUI
+
+struct ShowMenuButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowMenuButtonView()
+}
